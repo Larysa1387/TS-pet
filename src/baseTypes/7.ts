f18949763b0fd408ddcef7e46e97acb638d5baad
larysa1387 <kaliuzhna.off@gmail.com>
2024-02-21 15:25:41 +0200

    fixed baseTypes

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,29 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDay {
+  Mn = 'Monday',
+  Tue = 'Tuesday',
+  Wd = 'Wednesday',
+  Th = 'Thursday',
+  Fr = 'Friday',
+}
+enum Weekend {
+  Sat = 'Saturday',
+  Sn = 'Sunday',
+}
+console.log(typeof WeekDay.Mn);
+
+// function isWeekend(day: WeekDay | Weekend): boolean {
+//   if (day === Weekend.Sat || day === Weekend.Sn) return false;
+//   else {
+//     return true;
+//   }
+// }
+// isWeekend(WeekDay.Fr);
+
+function isWeekend(day: WeekDay | Weekend): boolean {
+  return day === Weekend.Sat || day === Weekend.Sn;
+}
+console.log(isWeekend(WeekDay.Mn));
--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,8 +1,19 @@
 /*
-  Створіть новий тип даних, який підходить для цих двох об'єктів.
+Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+type data = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: 'open' | 'close';
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+const page1: data = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -10,14 +21,14 @@ const page1 = {
   details: {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
-  }
-}
+  },
+};
 
-const page2 = {
+const page2: data = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
-}
+};
 
 export {};
